f24d894a91da8047d1c93d878cbef27c41a69d6d
Tarun085 <tarun1749.be20@chitkara.edu.in>
2022-08-11 15:48:09 +0530

    blog-home

--- js/blog.js
+++ js/blog.js
@@ -20,4 +20,27 @@ document.querySelector('#close-search').onclick = () =>{
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
-}
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
